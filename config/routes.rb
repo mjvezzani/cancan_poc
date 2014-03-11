89b1761b6c5b478a1705ff091e44f204c83f2a8d
Michael Vezzani <mjvezzani@gmail.com>
2014-03-11 08:13:29 -0400

    Create blog. Create db:seed data. Add 'new post' button to root path.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 CancanPoc::Application.routes.draw do
+  resources :posts
+
   devise_for :admins
   get "main_pages/index"
   # The priority is based upon order of creation: first created -> highest priority.
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+5.times do |i|
+  Post.create(title: "Post #{i}", body: "This is the body for post #{i}")
+end
