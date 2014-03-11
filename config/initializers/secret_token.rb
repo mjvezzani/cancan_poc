76a8b2179f7856fb926e3ad7f141e6e413f8af57
Michael Vezzani <mjvezzani@gmail.com>
2014-03-11 07:30:11 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CancanPoc::Application.config.secret_key_base = 'f39ad6655c3a521aae6ec7c8b388b8fdb496133f3d30cd341b1fefd479cff662471b1777f0173a9ae1fdbf40830a4a0dea8fc12dcf13104f0806d261aac84b8c'
